366f2ee04c72d54ff3d2b9ae02c9611f37fb7aff
maslov-maksym-serhiiovych <maslov.maksym.serhiiovych@gmail.com>
2025-10-29 01:53:55 +0200

    feat(users): integrate ProfileInline and customize CustomUser admin

--- nutrifit-backend/users/admin.py
+++ nutrifit-backend/users/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, Profile
 
-# Register your models here.
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+    fk_name = 'user'
+
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (ProfileInline,)
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+    readonly_fields = ('date_joined', 'last_login')
+    fieldsets = UserAdmin.fieldsets
+    add_fieldsets = UserAdmin.add_fieldsets
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
