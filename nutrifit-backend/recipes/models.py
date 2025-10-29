34a8603ccd28f960b8d627310ac2e4a863b91c35
maslov-maksym-serhiiovych <maslov.maksym.serhiiovych@gmail.com>
2025-10-29 23:17:53 +0200

    fix(recipes): fix Category verbose_name_plural

--- nutrifit-backend/recipes/models.py
+++ nutrifit-backend/recipes/models.py
@@ -6,6 +6,9 @@ class Category(models.Model):
     name = models.CharField(max_length=100, unique=True, verbose_name="Category name")
     slug = models.SlugField(max_length=100, unique=True)
 
+    class Meta:
+        verbose_name_plural = "Categories"
+
     def __str__(self):
         return self.name
 
