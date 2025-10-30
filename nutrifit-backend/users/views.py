45d5a8e2e53116acd6c9aded5e1d5205dcc551b5
maslov-maksym-serhiiovych <maslov.maksym.serhiiovych@gmail.com>
2025-10-30 13:58:31 +0200

    feat(auth): add user registration endpoint and JWT token routes

--- nutrifit-backend/users/views.py
+++ nutrifit-backend/users/views.py
@@ -1,7 +1,7 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, generics
 from .models import CustomUser
 from .permissions import IsOwner
-from .serializers import CustomUserSerializer
+from .serializers import CustomUserSerializer, RegisterSerializer
 
 
 class UserProfileViewSet(viewsets.ModelViewSet):
@@ -12,3 +12,9 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         user = self.request.user
         return CustomUser.objects.all() if user.is_staff else CustomUser.objects.filter(pk=user.pk)
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = RegisterSerializer
