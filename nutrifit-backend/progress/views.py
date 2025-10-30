7b65615d8ffef2df90185ab92bf8f909775940d2
maslov-maksym-serhiiovych <maslov.maksym.serhiiovych@gmail.com>
2025-10-30 16:20:09 +0200

    feat(progress): add serializers and secure ViewSets for progress logs

--- nutrifit-backend/progress/views.py
+++ nutrifit-backend/progress/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import WeightLog, CalorieLog
+from .serializers import WeightLogSerializer, CalorieLogSerializer
 
-# Create your views here.
+
+class BaseProgressViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class WeightLogViewSet(BaseProgressViewSet):
+    queryset = WeightLog.objects.all()
+    serializer_class = WeightLogSerializer
+
+
+class CalorieLogViewSet(BaseProgressViewSet):
+    queryset = CalorieLog.objects.all()
+    serializer_class = CalorieLogSerializer
