c3a3715e01e3d6b0c8c3e0b23fd28d55dfda417e
maslov-maksym-serhiiovych <maslov.maksym.serhiiovych@gmail.com>
2025-11-01 14:54:36 +0200

    refactor(users): simplify code

--- nutrifit-backend/users/models.py
+++ nutrifit-backend/users/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True, verbose_name="email address")
+    email = models.EmailField(unique=True)
 
     def __str__(self):
         return self.username
@@ -30,15 +30,14 @@ class Profile(models.Model):
         HIGH = "H", "High"
 
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
-    goal = models.TextField(blank=True, null=True, verbose_name="Goal")
-    weight = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True, verbose_name="Weight")
-    height = models.PositiveIntegerField(blank=True, null=True, verbose_name="Height")
-    age = models.PositiveIntegerField(blank=True, null=True, verbose_name="Age")
-    gender = models.CharField(max_length=1, choices=Gender.choices, blank=True, null=True, verbose_name="Gender")
+    goal = models.TextField(null=True, blank=True)
+    weight = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    height = models.PositiveIntegerField(null=True, blank=True)
+    age = models.PositiveIntegerField(null=True, blank=True)
+    gender = models.CharField(max_length=1, choices=Gender.choices, null=True, blank=True)
 
-    activity_level = models.CharField(max_length=1, choices=ActivityLevel.choices, default=ActivityLevel.LOW,
-                                      blank=True, null=True,
-                                      verbose_name="Activity level")
+    activity_level = models.CharField(max_length=1, choices=ActivityLevel.choices, default=ActivityLevel.LOW, null=True,
+                                      blank=True)
 
     def __str__(self):
         return f"{self.user.username}'s profile"
